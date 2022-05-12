10191d03f65957e4a1fe4155e3932b5fd5d1bf31
Senyrhugo <105436319+Senyrhugo@users.noreply.github.com>
2022-05-13 06:04:57 +0800

    Add files via upload

---
+++ novelsite/build/classes/Purchase/PurchaseInfo.java
@@ -0,0 +1,40 @@
+package Purchase;
+
+public class PurchaseInfo {
+	int UID;
+	int ID;
+	int RechargeMoney;
+	int Token;
+	int PurchaseTime;
+	public int getUID() {
+		return UID;
+	}
+	public void setUID(int uID) {
+		UID = uID;
+	}
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public int getRechargeMoney() {
+		return RechargeMoney;
+	}
+	public void setRechargeMoney(int rechargeMoney) {
+		RechargeMoney = rechargeMoney;
+	}
+	public int getToken() {
+		return Token;
+	}
+	public void setToken(int token) {
+		Token = token;
+	}
+	public int getPurchaseTime() {
+		return PurchaseTime;
+	}
+	public void setPurchaseTime(int purchaseTime) {
+		PurchaseTime = purchaseTime;
+	}
+
+}
